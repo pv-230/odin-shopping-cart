772c752c2c77e02e75a90881e4bd4acfe0cc793f
pv-230 <81388791+pv-230@users.noreply.github.com>
2022-12-16 09:46:33 -0500

    Update navbar styling

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,17 +1,37 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './navbar.css';
+import cartUrl from './cart.svg';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <NavLink
+            to="/"
+            className={({ isActive }) => {
+              if (isActive) return 'navbar__link navbar__link_active';
+              return 'navbar__link';
+            }}
+          >
+            Home
+          </NavLink>
         </li>
         <li>
-          <Link to="/shop">Shop</Link>
+          <NavLink
+            to="/shop"
+            className={({ isActive }) => {
+              if (isActive) return 'navbar__link navbar__link_active';
+              return 'navbar__link';
+            }}
+          >
+            Shop
+          </NavLink>
         </li>
       </ul>
+      <div className="navbar__cart-icon">
+        <img className="navbar__cart-svg" src={cartUrl} alt="Shopping cart" />
+      </div>
     </nav>
   );
 }

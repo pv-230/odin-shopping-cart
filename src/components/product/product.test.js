ef09f24d8c6a7a31c51c75fa92630e43531513f7
pv-230 <81388791+pv-230@users.noreply.github.com>
2022-12-24 18:59:54 -0500

    Add tests for Product component

---
+++ src/components/product/product.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import Product from './product';
+
+//
+const testProps = {
+  id: 'test id',
+  name: 'test product',
+  price: 3.5,
+  addToCart: jest.fn(),
+};
+
+describe('product component', () => {
+  it('displays correct product information', () => {
+    render(<Product {...testProps} />);
+    expect(screen.getByAltText(testProps.name)).toBeInTheDocument();
+    expect(screen.getByText('test product')).toBeInTheDocument();
+    expect(screen.getByText('$3.50')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Add to cart' })).toBeInTheDocument();
+  });
+
+  it('calls addToCart when button is clicked', () => {
+    render(<Product {...testProps} />);
+    const addBtn = screen.getByRole('button', { name: 'Add to cart' });
+    userEvent.click(addBtn);
+    expect(testProps.addToCart).toBeCalled();
+  });
+});

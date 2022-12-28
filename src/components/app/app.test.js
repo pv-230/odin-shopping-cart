2e22f41a9bfcfa862e5629c42933c08b451676db
pv-230 <81388791+pv-230@users.noreply.github.com>
2022-12-27 19:20:53 -0500

    Add more App component tests

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -46,14 +46,15 @@ jest.mock(
                 <span data-testid={`${productKey}-name`}>{product.name}</span>
                 <input
                   type="number"
-                  data-testid={`${productKey}-quantity`}
+                  data-testid={`${productKey}-quantity-input`}
                   value={product.quantity || ''}
                   onChange={(e) =>
                     updateItemQuantity(productKey, Math.abs(Math.floor(Number(e.target.value))))
                   }
                 />
+                <span data-testid={`${productKey}-quantity`}>{product.quantity}</span>
                 <button
-                  data-testid="remove-btn"
+                  data-testid={`${productKey}-remove-btn`}
                   onClick={() => removeFromCart(productKey)}
                 ></button>
               </li>
@@ -85,7 +86,7 @@ jest.mock('react-router-dom', () => ({
   Outlet: ({ context }) =>
     productsArr.map(([productKey, product]) => (
       <button
-        data-testid={`${productKey}-btn`}
+        data-testid={`${productKey}-add-btn`}
         key={productKey}
         onClick={() => context.addToCart(productKey)}
       ></button>
@@ -95,15 +96,49 @@ jest.mock('react-router-dom', () => ({
 describe('app component', () => {
   it('adds product to cart', async () => {
     render(<App />);
-    const addBtn = screen.getByTestId('key1-btn');
     const cartBtn = screen.getByTestId('cart-btn');
 
+    // ensure cart is empty
     expect(screen.queryByTestId('cart')).not.toBeInTheDocument();
     expect(cartBtn.textContent).toBe('0');
-    userEvent.click(addBtn);
+
+    // add item
+    userEvent.click(screen.getByTestId('key1-add-btn'));
     expect(cartBtn.textContent).toBe('1');
-    // TODO: figure out why cart doesn't open
-    // userEvent.click(cartBtn);
-    // expect(screen.getByTestId('cart')).toBeInTheDocument();
+
+    // check cart
+    userEvent.click(cartBtn);
+    expect(screen.getByTestId('cart')).toBeInTheDocument();
+    expect(screen.getByTestId('key1-name').textContent).toBe('item1');
+    expect(screen.getByTestId('key1-quantity').textContent).toBe('1');
+  });
+
+  it('removes items from cart', () => {
+    render(<App />);
+
+    // add to cart
+    userEvent.click(screen.getByTestId('key2-add-btn'));
+    userEvent.click(screen.getByTestId('cart-btn'));
+    expect(screen.getByTestId('key2-name').textContent).toBe('item2');
+
+    // remove from cart
+    userEvent.click(screen.getByTestId('key2-remove-btn'));
+    expect(screen.queryByTestId('key2-name')).not.toBeInTheDocument();
+  });
+
+  it('updates quantity of a cart item', () => {
+    render(<App />);
+
+    // add to cart
+    userEvent.click(screen.getByTestId('key2-add-btn'));
+    expect(screen.getByTestId('cart-btn').textContent).toBe('1');
+    userEvent.click(screen.getByTestId('cart-btn'));
+    expect(screen.getByTestId('key2-name').textContent).toBe('item2');
+    expect(screen.getByTestId('key2-quantity').textContent).toBe('1');
+
+    // update quantity
+    userEvent.type(screen.getByTestId('key2-quantity-input'), '0');
+    expect(screen.getByTestId('cart-btn').textContent).toBe('10');
+    expect(screen.getByTestId('key2-quantity').textContent).toBe('10');
   });
 });

288397135b587180b96a8338e424f7d5d3945fa6
pv-230 <81388791+pv-230@users.noreply.github.com>
2023-01-05 13:05:09 -0500

    Update home page design

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,7 +1,25 @@
+import { Link } from 'react-router-dom';
 import './home.css';
 
 function Home() {
-  return <div className="home">This is the home page.</div>;
+  return (
+    <div className="home">
+      <div className="welcome-box">
+        <div className="welcome-box__left">
+          <div className="welcome-box__text">
+            <div className="welcome-box__header">WELCOME</div>
+            <div className="welcome-box__text-description">
+              Browse our selection of fresh and organic fruits.
+            </div>
+            <Link className="welcome-box__shop-btn" to="/shop">
+              Shop now
+            </Link>
+          </div>
+        </div>
+        <div className="welcome-box__image"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
